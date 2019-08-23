29a4f4d7c4cf3d58e0efc657a58346ae0a64fb55
Fredrick Barrett <f.barrett12@gmail.com>
2019-08-23 17:07:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,9 +31,9 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   
   if hash[key]
-    hash[key] += 1
+   return hash[key] += 1
     else
-    hash[key] = 1
+    hash[key] = "hello"
 end
 
 end
